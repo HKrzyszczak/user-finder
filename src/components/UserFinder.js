5a77f1fd59bbcca30fa416be155a3d0dcfdbd98f
Henryk Krzyszczak <henryk.krzyszczak@intive.com>
2024-02-14 07:11:52 +0100

    use context in class components

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -1,53 +1,50 @@
-import { Fragment, useState, useEffect, Component } from 'react'
+import { Fragment, Component } from 'react';
 
-import Users from './Users'
-import classes from './UserFinder.module.css'
-
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' }
-]
+import Users from './Users';
+import classes from './UserFinder.module.css';
+import UsersContext from '../store/users-context';
 
 class UserFinder extends Component {
-  constructor () {
-    super()
+  static contextType = UsersContext;
+
+  constructor() {
+    super();
     this.state = {
       filteredUsers: [],
       searchTerm: ''
-    }
+    };
   }
 
-  componentDidUpdate (prevProps, prevState) {
+  componentDidMount() {
+    this.setState({ filteredUsers: this.context.users });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
     if (prevState.searchTerm !== this.state.searchTerm) {
       this.setState({
-        filteredUsers: DUMMY_USERS.filter(user =>
+        filteredUsers: this.context.users.filter(user =>
           user.name.includes(this.state.searchTerm)
         )
-      })
+      });
     }
   }
 
-  componentDidMount () {
-    this.setState({ filteredUsers: DUMMY_USERS })
-  }
-
-  searchChangeHandler (event) {
+  searchChangeHandler(event) {
     this.setState({
       searchTerm: event.target.value
-    })
+    });
   }
 
-  render () {
+  render() {
     return (
       <Fragment>
         <div className={classes.finder}>
-          <input type='search' onChange={this.searchChangeHandler.bind(this)} />
+          <input type="search" onChange={this.searchChangeHandler.bind(this)} />
         </div>
         <Users users={this.state.filteredUsers} />
       </Fragment>
-    )
+    );
   }
 }
 
-export default UserFinder
+export default UserFinder;
